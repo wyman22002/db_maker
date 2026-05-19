c2e4159309362577f698a12baa2c79d46cca7ff5
wyman22002 <tannerw2002@hotmail.com>
2026-05-19 16:38:41 -0600

    Got template working

--- db_maker/db_app/views.py
+++ db_maker/db_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world! - Ralsei")
+    return render(request, 'db_app/home.html')
