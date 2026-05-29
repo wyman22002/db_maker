73ac06d21bfadcbe8ee9dd81a1f1076cb4660015
wyman22002 <tannerw2002@hotmail.com>
2026-05-28 19:18:03 -0600

    Lots of changes

--- db_maker/db_app/views.py
+++ db_maker/db_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from .models import TODOItem
 
 # Create your views here.
 def home(request):
     return render(request, 'db_app/home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def todos(request):
+    items = TODOItem.objects.all()
+    return render(request, 'todos.html', {'todos': items})
--- db_maker/db_maker/settings.py
+++ db_maker/db_maker/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
